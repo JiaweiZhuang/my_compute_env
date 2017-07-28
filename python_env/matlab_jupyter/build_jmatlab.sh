ac162eb8e2b444591e06bcba5c172d8554037545
Jiawei Zhuang <zhuangjw@Jiaweis-MacBook-Pro.local>
2017-07-28 09:45:32 -0400

    fix jupyter matlab setup scripts
    Signed-off-by: Jiawei Zhuang <zhuangjw@Jiaweis-MacBook-Pro.local>

--- python_env/matlab_jupyter/build_jmatlab.sh
+++ python_env/matlab_jupyter/build_jmatlab.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-conda env create -vv -f ./matlab_jupyter.yml
+conda env create -vv -f ./jmatlab.yml
 
 # https://github.com/Calysto/matlab_kernel
-source activate matlab_jupyter
+source activate jmatlab
 python -m matlab_kernel install
 
 # Your MATLAB path
 MATLAB_PATH="/Applications/MATLAB_R2017a.app"
 
 cd $MATLAB_PATH/extern/engines/python/
-./setup.py
+python setup.py install
